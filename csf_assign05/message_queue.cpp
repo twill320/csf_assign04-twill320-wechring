d4d3b58bd940ddffb472f5f1225aa72e1cb40a3c
twill320 <twill320@jh.edu>
2025-04-28 13:52:31 -0400

    no significant changes

--- csf_assign05/message_queue.cpp
+++ csf_assign05/message_queue.cpp
@@ -34,7 +34,6 @@ Message *MessageQueue::dequeue() {
   //       to be available, return nullptr if no message is available
 
   // TODO: remove the next message from the queue, return it
-  delete this; // used dynamically allocated input to enqueue in room.cpp, need to delete at some point in dequeue after value is used
   Message *msg = nullptr;
   return msg;
 }

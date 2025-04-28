5236ba51c8f7dd8e7a365383c262026d3025d45c
twill320 <twill320@jh.edu>
2025-04-28 11:04:12 -0400

    defined member functions for Room data type

--- csf_assign05/room.cpp
+++ csf_assign05/room.cpp
@@ -7,20 +7,32 @@
 Room::Room(const std::string &room_name)
   : room_name(room_name) {
   // TODO: initialize the mutex
+  ::pthread_mutex_init(&lock, NULL);
 }
 
 Room::~Room() {
   // TODO: destroy the mutex
+  ::pthread_mutex_destroy(&lock);
 }
 
 void Room::add_member(User *user) {
   // TODO: add User to the room
+  members.insert(user);
 }
 
 void Room::remove_member(User *user) {
   // TODO: remove User from the room
+  members.erase(user);
 }
 
 void Room::broadcast_message(const std::string &sender_username, const std::string &message_text) {
   // TODO: send a message to every (receiver) User in the room
+  for (std::set<User *>::iterator it = members.begin(); it != members.end(); ++it) {
+    User *user = *it;
+    if (user->username == sender_username){
+      continue;
+    }
+    struct Message *msg = new Message{ TAG_DELIVERY, message_text };
+    user->mqueue.enqueue(msg);
+  }
 }

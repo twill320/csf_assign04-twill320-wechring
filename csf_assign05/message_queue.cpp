c6b0955d28c390768646fa9cdb11b63e42b45857
twill320 <twill320@jh.edu>
2025-04-28 23:02:16 -0400

    added definitions of member functions and constructor and destructor

--- csf_assign05/message_queue.cpp
+++ csf_assign05/message_queue.cpp
@@ -4,10 +4,20 @@
 
 MessageQueue::MessageQueue() {
   // TODO: initialize the mutex and the semaphore
+  ::pthread_mutex_lock(&m_lock);
+  ::pthread_mutex_unlock(&m_lock);
+  ::pthread_mutex_init(&m_lock, NULL);
+  ::sem_init(&m_avail, 0, 0);
 }
 
 MessageQueue::~MessageQueue() {
   // TODO: destroy the mutex and the semaphore
+  ::pthread_mutex_lock(&m_lock);
+  ::pthread_mutex_unlock(&m_lock);
+  ::sem_post(&m_avail);
+  ::sem_destroy(&m_avail);
+
+  ::pthread_mutex_destroy(&m_lock);
 }
 
 void MessageQueue::enqueue(Message *msg) {
@@ -15,6 +25,12 @@ void MessageQueue::enqueue(Message *msg) {
 
   // be sure to notify any thread waiting for a message to be
   // available by calling sem_post
+
+  ::pthread_mutex_lock(&m_lock);
+  m_messages.push_back(msg);
+  ::pthread_mutex_unlock(&m_lock);
+
+  ::sem_post(&m_avail);
 
 }
 
@@ -32,8 +48,15 @@ Message *MessageQueue::dequeue() {
 
   // TODO: call sem_timedwait to wait up to 1 second for a message
   //       to be available, return nullptr if no message is available
+  ::sem_wait(&m_avail);
 
   // TODO: remove the next message from the queue, return it
   Message *msg = nullptr;
+  ::pthread_mutex_lock(&m_lock);
+  if (!m_messages.empty()) {
+    msg = m_messages.front();
+    m_messages.pop_front();
+  }
+  ::pthread_mutex_unlock(&m_lock);
   return msg;
 }
